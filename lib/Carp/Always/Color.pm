18ae31ad947773d36b6667b6c44aa841147c0dd5
Jesse Luehrs <doy@cpan.org>
2010-04-30 23:24:22 +0000

    Import of DOY/Carp-Always-Color-0.01 from CPAN.
    gitpan-cpan-distribution: Carp-Always-Color
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DOY/Carp-Always-Color-0.01.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

---
+++ lib/Carp/Always/Color.pm
@@ -0,0 +1,92 @@
+package Carp::Always::Color;
+BEGIN {
+  $Carp::Always::Color::VERSION = '0.01';
+}
+BEGIN {
+    if (-t *STDERR) {
+        require Carp::Always::Color::Term;
+    }
+    else {
+        require Carp::Always::Color::HTML;
+    }
+}
+
+=head1 NAME
+
+Carp::Always::Color - Carp::Always, but with color
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+  use Carp::Always::Color;
+
+or
+
+  perl -MCarp::Always::Color -e'sub foo { die "foo" } foo()'
+
+=head1 DESCRIPTION
+
+Stack traces are hard to read when the messages wrap, because it's hard to tell
+when one message ends and the next message starts. This just colors the first
+line of each stacktrace, based on whether it's a warning or an error. If
+messages are being sent to a terminal, it colors them with terminal escape
+codes, otherwise it colors them with HTML (ideas for more intelligent behavior
+here are welcome).
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-carp-always-color at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Carp-Always-Color>.
+
+=head1 SEE ALSO
+
+L<Carp::Always>
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Carp::Always::Color
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Carp-Always-Color>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Carp-Always-Color>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Carp-Always-Color>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Carp-Always-Color>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
---
+++ lib/Carp/Always/Color/Term.pm
@@ -0,0 +1,73 @@
+package Carp::Always::Color::Term;
+BEGIN {
+  $Carp::Always::Color::Term::VERSION = '0.01';
+}
+use Carp::Always;
+
+=head1 NAME
+
+Carp::Always::Color::Term - Carp::Always, but with terminal color
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+  use Carp::Always::Color::Term;
+
+or
+
+  perl -MCarp::Always::Color::Term -e'sub foo { die "foo" } foo()'
+
+=head1 DESCRIPTION
+
+Like L<Carp::Always::Color>, but forces ANSI terminal code coloring, regardless
+of where STDERR is pointing to.
+
+=cut
+
+BEGIN { $Carp::Internal{(__PACKAGE__)}++ }
+
+sub _die {
+    eval { Carp::Always::_die(@_) };
+    my $err = $@;
+    $err =~ s/(.*)/\e[31m$1\e[m/;
+    die $err;
+}
+
+sub _warn {
+    my $warning;
+    {
+        local $SIG{__WARN__} = sub { $warning = $_[0] };
+        Carp::Always::_warn(@_);
+    }
+    $warning =~ s/(.*)/\e[33m$1\e[m/;
+    warn $warning;
+}
+
+my %OLD_SIG;
+BEGIN {
+    @OLD_SIG{qw(__DIE__ __WARN__)} = @SIG{qw(__DIE__ __WARN__)};
+    $SIG{__DIE__} = \&_die;
+    $SIG{__WARN__} = \&_warn;
+}
+
+END {
+    @SIG{qw(__DIE__ __WARN__)} = @OLD_SIG{qw(__DIE__ __WARN__)};
+}
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
